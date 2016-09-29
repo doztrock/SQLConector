9f335d8ab25079a4567de448bcaeff8f92732b78
Ivan Botero <ivan.botero@protonmail.ch>
2016-09-29 13:18:24 -0500

    MySQL OK

--- conector-test.cpp
+++ conector-test.cpp
@@ -1,43 +1,9 @@
-#include <iostream>
 #include <cstdlib>
-
-#include <ctime>
-#include <sstream>
-#include <unistd.h>
 
 #include "Conector.hpp"
 
 using namespace std;
 
-string generarNumero(void) {
-
-    stringstream numero;
-    srand(time(NULL));
-
-    numero << rand() % 1000000 + 100000;
-
-    return numero.str();
-}
-
 int main(void) {
-
-    Conector *conector = new Conector(MYSQL_DB);
-    conector->conectar("localhost", "root", "Slptshr369*", "prueba");
-
-    //    conector->consulta("select * from cliente");
-    //
-    //    cout << "Se encontraron: " << conector->obtenerCantidadResultados() << " resultados." << endl;
-    //
-    //    Resultado resultado;
-    //
-    //    while (conector->obtenerResultado(resultado)) {
-    //        cout << resultado["id_cliente"] << " <=> " << resultado["nombre"] << endl;
-    //        cout << endl << endl;
-    //    }
-    //
-    //    cout << "Escape: " << conector->escape("SELECT ID_CLIENTE FROM CLIENTE, USUARIO WHERE DATO = 'UNO%'") << endl;
-    //
-    conector->desconectar();
-
     return EXIT_SUCCESS;
 }
